e5149467f9d3cfff943ba3f7093114a0b0d307e5
KrsPatrick <lesiewicz.patrick@gmail.com>
2022-07-06 12:08:19 +0200

    update

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URL = "mongodb+srv://krspatrick:TqUTdCD5cWuvAWSw@krsshop.glqdvoa.mongodb.net/?retryWrites=true&w=majority" // process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL
 
 mongoose.connection.once('open', () => {
     console.log("db is open!");
